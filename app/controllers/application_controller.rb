0c40113403a8b075ff4999b72c3659de5ff2e7b4
石斌 <inselfteaching@gmail.com>
2026-05-25 10:16:21 +0800

    浏览器检测

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern unless Rails.env.test?
+  # allow_browser versions: :modern unless Rails.env.test?
 
   before_action :set_locale
   before_action :track_visitor
