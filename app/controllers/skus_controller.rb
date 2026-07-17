e0faf576d4461e4b7a588b724bf0f0175ffd65d9
石斌 <inselfteaching@gmail.com>
2026-07-17 12:41:17 +0800

    add page

--- app/controllers/skus_controller.rb
+++ app/controllers/skus_controller.rb
@@ -1,6 +1,12 @@
 class SkusController < ApplicationController
   def show
-    @sku = Sku.includes(category: { parent: { parent: :parent } }).find(params[:id])
+    @sku = Sku.includes(category: { parent: { parent: :parent } }).find_by(id: params[:id])
+    
+    if @sku.nil?
+      redirect_to all_products_path, alert: t('categories.no_skus_title')
+      return
+    end
+
     @kind = @sku.category.category_kind
   end
 end
